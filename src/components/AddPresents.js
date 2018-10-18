c83a37abcf3a8d651d8626595fff111a45e254d6
Marquet Reid <you@example.com>
2018-10-17 23:54:08 -0400

    wrote code to start out Add/EditPresents

---
+++ src/components/AddPresents.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+
+class AddPresents extends Component {
+
+  render() {
+    return(
+      <div></div>
+    )
+  }
+}
+
+export default AddPresents
---
+++ src/components/EditPresents.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+
+class EditPresents extends Component {
+
+  render() {
+    return(
+      <div></div>
+    )
+  }
+}
+
+export default EditPresents
